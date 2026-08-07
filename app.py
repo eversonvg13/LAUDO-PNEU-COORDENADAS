a04b33ad3430fad8d05fa81d8a05cd3d3d756216
Everson Veloso <92476038+eversonvg13@users.noreply.github.com>
2026-08-07 16:08:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -623,10 +623,10 @@ if st.button("Salvar Laudo na Planilha", use_container_width=True):
             sucessos = 0
             erros = []
 
-            st.write(f"DEBUG local: '{pneu.get('local', 'NÃO ENCONTRADO')}'")
+            
             # Percorre cada pneu individualmente da lista e salva na planilha
             for pneu in pneus_para_salvar:
-        
+                st.write(f"DEBUG local: '{pneu.get('local', 'NÃO ENCONTRADO')}'")
                 try:
                     sucesso, mensagem = salvar_no_onedrive(pneu)
                     if sucesso:
